27ff1c45f94a3f0f3f65fce5711e65b1c1924231
Albert Soriano <al.soriano.thais@gmail.com>
2017-12-11 15:17:32 -0500

    change images

---
+++ js/ajax-contact.js
@@ -0,0 +1,46 @@
+$(function () {
+
+    // Get the form.
+    var form = $('#main_contact_form');
+
+    // Get the messages div.
+    var formMessages = $('#success_fail_info');
+
+    // Set up an event listener for the contact form.
+    $(form).submit(function (e) {
+        // Stop the browser from submitting the form.
+        e.preventDefault();
+
+        // Serialize the form data.
+        var formData = $(form).serialize();
+        var $submit = $('#btn-send');
+
+        $submit.text('Enviando...').prop('disabled', true);
+        $(formMessages).removeClass('success error').html('');
+        $.ajax({
+                type: 'POST',
+                url: $(form).attr('action'),
+                data: formData,
+                dataType: 'json'
+            })
+            .done(function (response) {
+                // Make sure that the formMessages div has the 'success' class.
+                $(formMessages)
+                    .removeClass('error')
+                    .addClass('success')
+                    .html(response.success_message);
+
+                $(form)[0].reset();
+            })
+            .fail(function (data, err) {
+                // Make sure that the formMessages div has the 'error' class.
+                console.log(err);
+                $(formMessages)
+                    .removeClass('success')
+                    .addClass('error')
+                    .text(data.responseJSON.error_message);
+            }).always(function() {
+                $submit.prop('disabled', false).text('Reservar o Cotizar Barra');
+            });
+    });
+});
